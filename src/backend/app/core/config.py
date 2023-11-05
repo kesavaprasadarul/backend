3d151be1fb0c97b1d3fe2b8552204be6b1253c8f
danielgruber99 <daniel.reisbach@gmx.de>
2023-11-05 21:16:57 +0100

    add database

--- src/backend/app/core/config.py
+++ src/backend/app/core/config.py
@@ -1,4 +1,5 @@
 """Configuration of Settings."""
+import pydantic as pyd
 from pydantic_settings import BaseSettings
 
 
@@ -6,7 +7,13 @@ class Settings(BaseSettings):
     """Containing settings by loading environment variables."""
 
     # GENERAL
-    API_V1_STR: str = "/api/v1"
+    API_V1_STR: str = pyd.Field(default="/api/v1")
+
+    # PostgreSQL Database
+    POSTGRES_DB: str = pyd.Field(default="fastapi")
+    POSTGRES_PASSWORD: str = pyd.Field(default="password")
+    POSTGRES_SERVER: str = pyd.Field(default="db")
+    POSTGRES_USER: str = pyd.Field(default="admin")
 
 
 settings = Settings()
---
+++ src/backend/app/db/database.py
@@ -0,0 +1,15 @@
+"""Create sqlalchemy database engine and session. Initialize declarative base for models/tables."""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from backend.app.core.config import settings as s
+
+DATABASE_URL = (
+    f"postgresql://{s.POSTGRES_USER}:{s.POSTGRES_PASSWORD}@{s.POSTGRES_SERVER}/{s.POSTGRES_DB}"
+)
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
+
+Base = declarative_base()
