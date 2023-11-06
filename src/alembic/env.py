3a145491fdc4bb56f08c4aa86ca6c9b3174ed53b
danielgruber99 <daniel.reisbach@gmx.de>
2023-11-06 20:13:14 +0100

    ignore pylint on alembic, isort in alembic
    Configured pyproject.toml to ignore pylint for alembic.

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -1,9 +1,9 @@
 import os
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from alembic import context
 from backend.app.db.database import Base
 
 # this is the Alembic Config object, which provides
