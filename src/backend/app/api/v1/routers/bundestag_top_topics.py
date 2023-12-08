b8217fd129f54ad4380561e3827e5d5754332737
danielgruber99 <daniel.reisbach@gmx.de>
2023-12-08 09:07:52 +0100

    impl. get top_topics from db & refactor BUNDESTAG_RESSORTS
    Implemented to get top_topics from database (so get endpoint will only
    read from db, if entries exists ok, else empty lists for each ressorts
    will be returned in top_topics  key).
    Refactored BUNDESTAG_RESSORST to have it globally in core available.

--- src/backend/app/api/v1/routers/bundestag_top_topics.py
+++ src/backend/app/api/v1/routers/bundestag_top_topics.py
@@ -4,30 +4,53 @@ import logging
 import fastapi
 
 from backend.app.api.v1.models.responses import BundestagTopTopicsApiResponse
-from backend.app.app_logic.landing_page.create_bundestag_top_topics import create_bundestag_top_topics
+from backend.app.app_logic.landing_page.create_bundestag_top_topics import (
+    create_bundestag_top_topics,
+)
+from backend.app.core.bundestag_ressorts import BUNDESTAG_RESSORTS
+from backend.app.services.bundestag_top_topics.service import BundestagTopTopicsService
 
 _logger = logging.getLogger(__name__)
 
 router = fastapi.APIRouter(prefix="/bundestag_top_topics", tags=["bundestag-top-topics"])
 
 
-@router.get(path='/')
+@router.get(path='/', response_model=BundestagTopTopicsApiResponse)
 def get_bundestag_top_topics(
-    month: int | None = None, year: int | None = None, election_period: int | None = None
+    month: int | None = None,
+    year: int | None = None,
+    election_period: int | None = None,
+    service: BundestagTopTopicsService = fastapi.Depends(BundestagTopTopicsService),
 ):
-    pass
-    # top_topics_per_ressort: dict[str, list[list]] = get_bundestag_top_topics_for_month(month, year)
+    bundestag_top_topics = service.query(
+        month=month,
+        year=year,
+        election_period=election_period,
+    )
 
-    # return BundestagTopTopicsApiResponse(
-    #     month=month, year=year, election_period=election_period, top_topics=top_topics_per_ressort
-    # )
+    top_topics_by_ressort: dict[str, list[list]] = {}
+
+    for ressort in BUNDESTAG_RESSORTS:
+        top_topics_by_ressort[ressort] = [
+            [top_topic.word, top_topic.value]
+            for top_topic in bundestag_top_topics
+            if top_topic.ressort == ressort
+        ]
+
+    api_response = BundestagTopTopicsApiResponse(
+        month=month, year=year, election_period=election_period, top_topics=top_topics_by_ressort
+    )
+
+    return api_response
 
 
 @router.post(path='/', response_model=BundestagTopTopicsApiResponse)
 def post_bundestag_top_topics(
     month: int | None = None, year: int | None = None, election_period: int | None = None
 ):
-    top_topics_per_ressort: dict[str, list[list]] = create_bundestag_top_topics(month, year, election_period)
+    top_topics_per_ressort: dict[str, list[list]] = create_bundestag_top_topics(
+        month, year, election_period
+    )
 
     return BundestagTopTopicsApiResponse(
         month=month, year=year, election_period=election_period, top_topics=top_topics_per_ressort
