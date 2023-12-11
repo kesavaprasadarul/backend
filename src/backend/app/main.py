a289972c6f4e54d257fd946aa282c1664a448e1a
Julian Streit <ga84xiq@mytum.de>
2023-12-11 15:44:43 +0100

    Fix CORS-Middleware to also accept prod frontend

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -8,7 +8,8 @@ from sched import scheduler
 from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_EXECUTED
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
-from fastapi import FastAPI
+from fastapi import FastAPI, middleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.app.api.v1.api import api_router
 from backend.app.core.config import settings
@@ -74,6 +75,21 @@ async def lifespan(app: FastAPI):
     app_scheduler.shutdown(wait=True)
 
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:80",
+    "http://localhost",
+]
+
+
 app = FastAPI(title="backend", openapi_url=f"{settings.API_V1_STR}/openapi.json", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix=f"{settings.API_V1_STR}")
