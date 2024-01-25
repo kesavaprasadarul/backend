c181035525956d56f3c3cfdd1597d8dd49dbe8c3
danielgruber99 <daniel.reisbach@gmx.de>
2024-01-25 22:03:18 +0100

    restructure top_topics table
    drop column id, and make month,year,election_period and ressort the
    primary key. So now upsert (create or update will work correctly on key
    if already exists).

--- src/backend/app/models/api/top_topics_model.py
+++ src/backend/app/models/api/top_topics_model.py
@@ -8,10 +8,10 @@ class TopTopics(Base):
     """Table attributes for Model/Relation/Table top topics."""
 
     __tablename__ = "top_topics"
-    id: Mapped[int] = mapped_column(primary_key=True)  # database id
+
+    month: Mapped[int] = mapped_column(primary_key=True, nullable=True)
+    year: Mapped[int] = mapped_column(primary_key=True, nullable=True)
+    election_period: Mapped[int] = mapped_column(primary_key=True, nullable=True)
     ressort: Mapped[str] = mapped_column(nullable=False)
     word: Mapped[str] = mapped_column(nullable=False)
     value: Mapped[str] = mapped_column(nullable=False)
-    month: Mapped[int] = mapped_column(nullable=True)
-    year: Mapped[int] = mapped_column(nullable=True)
-    election_period: Mapped[int] = mapped_column(nullable=True)
